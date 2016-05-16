8b315efb3b9b665017239c7c9721f7d24b6017e2
tc033655 <tridi.etce@gmail.com>
2016-05-16 13:16:28 +0530

    Added recording nodes in temp files
    Now the script will capture the failed and successful hosts in the list

--- san_hostlist_path_check.sh
+++ san_hostlist_path_check.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Purpose : Checking SAN path status for hostlist
 # Author: Tridibesh Chakraborty
-#Version: 1.0
+# Email ID: tridi.etce@gmail.com
+#Version: 1.1
+# Change log: Adding logging of node name in the list file (05/16/2016)
 HOSTLIST=/export/incident/SAN_PATH_CHECK/bin/hostlist
 TMP_DIR=/export/incident/SAN_PATH_CHECK/logs
 
-#HOSTLIST=/home/tc033655/scripts/hostlist
-#TMP_DIR=/home/tc033655/hostlist_check
+
 if [ -d $TMP_DIR ]; then
-echo "Temp Directory is present"
+echo "Starting hostlist scan"
 else
 mkdir -p $TMP_DIR
 fi
@@ -17,6 +18,10 @@ LOGDIR=$TMP_DIR/`date "+%d_%m_%Y"`
 FINAL_DIR=$TMP_DIR/final
 FINAL_OUT=$FINAL_DIR/SAN_PATH_CHECK_REPORT_`date "+%d_%m_%Y"`.out
 TEMP_OUT=$LOGDIR/temp.out
+FAILED_HOST=$LOGDIR/failed_host
+SUC_HOST=$LOGDIR/succ_host
+ISSUE_HOST=$LOGDIR/issues
+PROPER_HOST=$LOGDIR/proper
 
 
 if [ -d $LOGDIR ]; then
@@ -45,9 +50,20 @@ SAN Path Check Report Generation status
 
 for HOST in `cat $HOSTLIST`
 do
+        echo "Checking node $HOST on `date "+%d_%m_%Y_%T"`" >>$TEMP_OUT
         /export/incident/SAN_PATH_CHECK/bin/san_path_check.sh $HOST 1>"$LOGDIR"/"$HOST".log 2>>$LOGDIR/error.log
         TOTAL_PATH=`grep TOTAL "$LOGDIR"/"$HOST".log | cut -d ":" -f 2`
+        if [ "$TOTAL_PATH" = "NA" ] || [ "$TOTAL_PATH" -eq 0 ]; then
+                echo $HOST >>$FAILED_HOST
+        else
+                echo $HOST >>$SUC_HOST
+        fi
         FAULTY_PATH=`egrep "FAULTY PATHS" "$LOGDIR"/"$HOST".log | cut -d ":" -f 2`
+        if [ "$FAULTY_PATH" -eq 0 ]; then
+                echo $HOST >>$PROPER_HOST
+        else
+                echo $HOST >>$ISSUE_HOST
+        fi
         INTACT_PATH=`grep INTACT "$LOGDIR"/"$HOST".log | cut -d ":" -f 2`
         #printf "%-30s %-15s %-15s %-10s\n" $HOST $TOTAL_PATH $INTACT_PATH $FAULTY_PATH >>$TEMP_OUT
 
